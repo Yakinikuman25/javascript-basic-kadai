e627616eba73fde0616ee2e951d7bc4d126676fb
Ryo <ryoryokozawa@gmail.com>
2026-01-16 05:49:12 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 体重
+let bodyWeight ;
+bodyWeight = 68;
+
+// 身長
+let userHeight ;
+userHeight = 1.7;
+
+// bmi
+let Bmi;
+Bmi = (bodyWeight/(userHeight*userHeight));
+
+// コンソールへの出力
+console.log(Bmi);
